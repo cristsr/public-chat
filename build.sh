e347d56244f9f3e2d1a3825655b74d3d25d59c45
cristsr <styven21121@gmail.com>
2022-05-20 21:01:44 -0500

    fix: image repository

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ docker rm public-chat
 
 echo "Run the new server"
 
-docker run -d -p 8080:8080 --name public-chat public-chat:latest
+docker run -d -p 8080:8080 --name public-chat cristian44/public-chat:latest
