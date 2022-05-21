9ca1ca896ffe68aac2fd4a188d4d16050d0b0638
cristsr <styven21121@gmail.com>
2022-05-20 20:56:57 -0500

    feat: build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "pull the latest image from registry"
+
+docker pull cristian44/public-chat:latest
+
+echo "Stop the old server"
+
+docker stop public-chat
+
+echo "Remove the old server"
+
+docker rm public-chat
+
+echo "Run the new server"
+
+docker run -d -p 8080:8080 --name public-chat public-chat:latest
